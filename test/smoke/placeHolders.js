b168eb4df75f347ea7b971cf05457f1245e167d2
Your Name <you@example.com>
2020-11-05 00:08:03 -0500

    created TC for placeholders

---
+++ test/smoke/placeHolders.js
@@ -0,0 +1,34 @@
+const sel = require ('../../data/selectors.json');
+const exp = require ('../../data/expected.json');
+
+describe('PlaceHolders', function () {
+
+    describe('Placeholders are displayed', function () {
+
+        it('TC-023 Placeholder for Name) ', function () {
+            browser.url('');
+            const text = $(sel.name).getAttribute('placeholder');
+            expect(text).toEqual(exp.placeHolderName);
+        });
+
+        it('TC-024 Placeholder for Age) ', function () {
+            const text = $(sel.age).getAttribute('placeholder');
+            expect(text).toEqual(exp.placeHolderAge);
+        });
+
+        it('TC-025 Placeholder for Type of story) ', function () {
+            const text = $(sel.placeHolderStory).getProperty('innerText');
+            expect(text).toEqual(exp.placeHolderStory);
+        });
+
+        it('TC-026 Placeholder for Upload an image) ', function () {
+            const text = $(sel.uploadButton).getProperty('innerText');
+            expect(text).toEqual(exp.placeHolderImage);
+        });
+
+        it('TC-027 Placeholder for Create button) ', function () {
+            const text = $(sel.create).getProperty('innerText');
+            expect(text).toEqual(exp.placeHolderCreate);
+        });
+    });
+})
