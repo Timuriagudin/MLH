7b7c82aeef984ec14b5b144a5f64fca7b6287669
Your Name <you@example.com>
2020-11-05 00:47:17 -0500

    modified data

--- test/smoke/mainFunctionality.js
+++ test/smoke/mainFunctionality.js
@@ -1,22 +1,22 @@
-const sel = require ('../../data/selectors.json');
-const data = require ('../../data/testData.json');
-const inputValues4 = require ('../../helpers/inputValues4');
+const sel = require('../../data/selectors.json');
+const data = require('../../data/testData.json');
+const inputValues4 = require('../../helpers/inputValues4');
 
 describe('Checking the main functionality', function () {
     describe('Happy path', function () {
 
         it('TC-021 Create button is clickable after 1-4 are filled in', function () {
             browser.url('');
-            inputValues4(data.name, data.gender.she, data.age, data.storyType);
+            inputValues4(data.name, data.gender.he, data.age, data.storyType.Comedy);
             const create = $(sel.create).isEnabled();
             expect(create).toEqual(true);
         });
     });
     describe('Other paths', function () {
 
-        it('TC-022 gender he is working', function () {
+        it('TC-022 gender he and type of the story comedy are working', function () {
             browser.url('');
-            inputValues4(data.name, data.gender.he, data.age, data.storyType);
+            inputValues4(data.name, data.gender.he, data.age, data.storyType.Comedy);
             $(sel.create).click();
             const btn = $(sel.tryAgain).isDisplayed();
             expect(btn).toEqual(true);
